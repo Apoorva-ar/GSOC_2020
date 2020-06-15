4b4619fde14d01eb8cc5517463412a32515bf7ac
apoorvaarora <apoorvaarora79@gmail.com>
2020-06-15 10:54:20 +0200

    Update PHY_Master.vhd

--- PHY_Master.vhd
+++ PHY_Master.vhd
@@ -11,23 +11,20 @@ ENTITY PHY_master IS
         DATA_SIZE : INTEGER := 16;
         FIFO_REQ  : BOOLEAN := True);
     PORT (
-        i_sys_clk : IN std_logic; -- system clock
-        i_sys_rst : IN std_logic; -- system reset
-
-        i_data : IN std_logic_vector(DATA_SIZE - 1 DOWNTO 0);  -- Input data
-        o_data : OUT std_logic_vector(DATA_SIZE - 1 DOWNTO 0); --output data
-
-        i_csn : IN std_logic; -- chip select for PHY master transaction Data IO
-        i_wr  : IN std_logic; -- Active Low Write, Active High Read
-        i_rd  : IN std_logic; -- Active Low Write, Active High Read
-
-        o_tx_ready : OUT std_logic; -- Transmitter ready, can write another data
-        o_rx_ready : OUT std_logic; -- Receiver ready, can read data
-
-        o_tx_error : OUT std_logic; -- Transmitter error
-        o_rx_error : OUT std_logic; -- Receiver error
-        o_intr     : OUT std_logic;
-
+        i_sys_clk      : IN std_logic;                                 -- system clock
+        i_sys_rst      : IN std_logic;                                 -- system reset
+        i_data         : IN std_logic_vector(DATA_SIZE - 1 DOWNTO 0);  -- Input data
+        o_data         : OUT std_logic_vector(DATA_SIZE - 1 DOWNTO 0); --output data
+        i_wr_tr_en     : IN std_logic;                                 -- write transaction enable
+        i_rd_tr_en     : IN std_logic;                                 -- read transaction enable
+        i_csn          : IN std_logic;                                 -- chip select for PHY master transaction Data IO
+        i_wr           : IN std_logic;                                 -- Active Low Write, Active High Read
+        i_rd           : IN std_logic;                                 -- Active Low Write, Active High Read
+        o_tx_ready     : OUT std_logic;                                -- Transmitter ready, can write another data
+        o_rx_ready     : OUT std_logic;                                -- Receiver ready, can read data
+        o_tx_error     : OUT std_logic;                                -- Transmitter error
+        o_rx_error     : OUT std_logic;                                -- Receiver error
+        o_intr         : OUT std_logic;
         i_cpol         : IN std_logic;                    -- CPOL value - 0 or 1
         i_cpha         : IN std_logic;                    -- CPHA value - 0 or 1 
         i_lsb_first    : IN std_logic;                    -- lsb first when '1' /msb first when 
@@ -36,10 +33,9 @@ ENTITY PHY_master IS
         i_setup_cycles : IN std_logic_vector(7 DOWNTO 0); --  setup time  in terms of i_sys_clk
         i_hold_cycles  : IN std_logic_vector(7 DOWNTO 0); --  hold time  in terms of i_sys_clk
         i_tx2tx_cycles : IN std_logic_vector(7 DOWNTO 0); --  interval between data transactions in terms of i_sys_clk
-
-        PHY_M_IO    : INOUT std_logic; -- LVDS bidirectional data link
-        o_sclk      : OUT std_logic;   -- Master clock
-        mosi_tri_en : OUT std_logic
+        PHY_M_IO       : INOUT std_logic;                 -- LVDS bidirectional data link
+        o_sclk         : OUT std_logic;                   -- Master clock
+        mosi_tri_en    : OUT std_logic
     );
 END PHY_master;
 
@@ -57,7 +53,9 @@ ARCHITECTURE PHY_master_rtl OF PHY_master IS
             i_hold_cycles  : IN std_logic_vector(7 DOWNTO 0);
             i_tx2tx_cycles : IN std_logic_vector(7 DOWNTO 0);
             i_cpol         : IN std_logic;
-            o_ss_start     : OUT std_logic;
+            write_tr_en    : IN std_logic;
+            read_tr_en     : IN std_logic;
+            o_ss_start     : OUT std_logic_vector(1 DOWNTO 0);
             o_sclk         : OUT std_logic
         );
     END COMPONENT;
@@ -84,14 +82,14 @@ ARCHITECTURE PHY_master_rtl OF PHY_master IS
             i_lsb_first : IN std_logic;                                 -- lsb first when '1' /msb first when -- '0'
             o_intr      : OUT std_logic;
             io_LVDS     : INOUT std_logic;
-            i_ssn       : IN std_logic; -- Slave Slect Active low
-            i_sclk      : IN std_logic  -- Clock from PHY Master clock gen
+            i_ssn       : IN std_logic_vector(1 DOWNTO 0); -- Slave Slect Active low
+            i_sclk      : IN std_logic                     -- Clock from PHY Master clock gen
         );
 
     END COMPONENT;
 
     SIGNAL sclk_i     : std_logic;
-    SIGNAL ss_start_i : std_logic;
+    SIGNAL ss_start_i : std_logic_vector(1 DOWNTO 0);
 BEGIN
 
     o_sclk <= sclk_i;
@@ -108,6 +106,8 @@ BEGIN
         i_hold_cycles  => i_hold_cycles,
         i_tx2tx_cycles => i_tx2tx_cycles,
         i_cpol         => i_cpol,
+        write_tr_en    => i_wr_tr_en,
+        read_tr_en     => i_rd_tr_en,
         o_ss_start     => ss_start_i,
         o_sclk         => sclk_i
     );
