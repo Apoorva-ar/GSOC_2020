5e2e075b961b2e82237cdcbb32927852afcf3073
apoorvaarora <apoorvaarora79@gmail.com>
2020-07-02 01:36:22 +0200

    Update packet_layer_tb.vhd

--- packet_layer_tb.vhd
+++ packet_layer_tb.vhd
@@ -1,4 +1,10 @@
-
+-----------------------------------------------------------------------------------
+-- This program is free software: you can redistribute it and/or
+-- modify it under the terms of the GNU General Public License
+-- as published by the Free Software Foundation, either version
+-- 2 of the License, or (at your option) any later version.
+----------------------------------------------------------------------------------
+-- testbench with Slave SPI (Master) peripheral test
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
