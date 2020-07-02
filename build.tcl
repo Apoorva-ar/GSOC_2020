57597691a5432a1e149a329495ce1a5757dfdf33
apoorvaarora <apoorvaarora79@gmail.com>
2020-07-02 18:56:33 +0200

    Update build.tcl

--- build.tcl
+++ build.tcl
@@ -4,7 +4,7 @@ prj_src add "/Packet_layer_tb.vhd" "/Packet_layer_SPI_tb.vhd" "/PHY_top_tb.vhd"
 "/PHY_clock_gen.vhd" "/PHY_Data_path.vhd" "/PHY_Master_Controller.vhd" "/PHY_Master.vhd"
 "/PHY_Slave.vhd" "/PHY_Slave_Controller.vhd" "/Packet_layer_Master.vhd"
 "/Packet_layer_Slave.vhd" "/Debug_core_Master.vhd" "/gray_counter.vhd" 
-"/asyn_fifo.vhd" "/sclk_gen.vhd" "/spi_data_path.vhd" "/spi_master.vhd" "/PHY_master.lpf"
+"/asyn_fifo.vhd" "/sclk_gen.vhd" "/spi_data_path.vhd" "/spi_master.vhd" "/PHY_master.lpf" "/FIFOx64.vhd"
 prj_project save
 prj_run Synthesis -impl impl1
 prj_run PAR -impl impl1
