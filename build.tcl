e415fd531fc77e66ceebf6b35cd5226802fa321d
apoorvaarora <apoorvaarora79@gmail.com>
2020-07-02 18:54:31 +0200

    Update build.tcl

--- build.tcl
+++ build.tcl
@@ -1,6 +1,6 @@
 
 prj_project new -name "Bidirectional_LVDS_Packet_proj" -impl "impl1" -dev LCMXO2-1200HC-5TG144C -synthesis "lse"
-prj_src add "/Packet_layer_tb.vhd" "/Packet_layer_SPI_tb.vhd" "/PHY_top_tb.vhd" 
+prj_src add "/Packet_layer_tb.vhd" "/Packet_layer_SPI_tb.vhd" "/PHY_top_tb.vhd"
 "/PHY_clock_gen.vhd" "/PHY_Data_path.vhd" "/PHY_Master_Controller.vhd" "/PHY_Master.vhd"
 "/PHY_Slave.vhd" "/PHY_Slave_Controller.vhd" "/Packet_layer_Master.vhd"
 "/Packet_layer_Slave.vhd" "/Debug_core_Master.vhd" "/gray_counter.vhd" 
