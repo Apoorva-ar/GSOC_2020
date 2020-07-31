f818919874587781e1b7aa3221b84a80c3353cc5
apoorvaarora <apoorvaarora79@gmail.com>
2020-07-31 11:34:24 +0200

    Update build.tcl

--- build.tcl
+++ build.tcl
@@ -3,8 +3,9 @@ prj_project new -name "Bidirectional_LVDS_Packet_proj" -impl "impl1" -dev LCMXO2
 prj_src add "/Packet_layer_tb.vhd" "/Packet_layer_SPI_tb.vhd" "/PHY_top_tb.vhd"
 "/PHY_sclk_gen.vhd" "/PHY_Data_path.vhd" "/PHY_Master_Controller.vhd" "/PHY_Master.vhd"
 "/PHY_Slave.vhd" "/PHY_Slave_Controller.vhd" "/Packet_layer_Master.vhd"
-"/Packet_layer_Slave.vhd" "/Debug_core_Master.vhd" "/gray_counter.vhd" 
-"/asyn_fifo.vhd" "/sclk_gen.vhd" "/spi_data_path.vhd" "/spi_master.vhd" "/PHY_master.lpf" "/FIFOx64.vhd"
+"/Packet_layer_Slave.vhd" "/PHY_master.lpf" "/FIFOx64.vhd" "/Scheduler_Master.vhd" "/command_fifo_1.vhd"
+"/command_fifo_2.vhd" "/command_fifo_3.vhd" "/data_fifo_1.vhd" "/data_fifo_2.vhd" "/data_fifo_3.vhd"
+"/fifo_read_1.vhd" "/fifo_read_2.vhd" "/fifo_read_3.vhd"
 prj_project save
 prj_run Synthesis -impl impl1
 prj_run PAR -impl impl1
